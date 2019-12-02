75e1a3b4e6d02c5e57b2210a489ab785d4b73b62
schmidgallm <schmidgallm@outlook.com>
2019-12-02 16:20:10 -0700

    express validator and post user route begin

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.get('/', async (req, res) => {
-	try {
-		res.status(200).json({ msg: 'hello there' });
-	} catch (err) {
-		console.warn(err);
-		res.status(401).json({ msg: 'Not authorized' });
+router.get(
+	'/',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please include valid email').isEmail(),
+		check('password', 'password must be more than 6 characters').isLength({ min: 6 })
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ errors: errors.array() });
+			}
+		} catch (err) {
+			console.warn(err);
+			res.status(401).json({ msg: 'Not authorized' });
+		}
 	}
-});
+);
+
+router.post('/', async (req, res) => {});
 
 module.exports = router;

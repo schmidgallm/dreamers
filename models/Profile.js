5d7360b138d251dbd9a6a48d84f66449edc79ada
schmidgallm <schmidgallm@outlook.com>
2019-12-05 21:53:59 -0700

    init profile model

--- models/Profile.js
+++ models/Profile.js
@@ -2,6 +2,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProfileSchema = new Schema({});
+const ProfileSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  bio: {
+    type: String
+  },
+  favoriteBook: {
+    type: String
+  },
+  favoriteAuthor: {
+    type: String
+  }
+});
 
 module.exports = Profile = mongoose.model("profiles", ProfileSchema);

1044f5dd3257edbfc6aea7f73c97ea6b61b32e49
schmidgallm <schmidgallm@outlook.com>
2019-12-17 01:14:44 -0700

    add post and delete routes for comments in prompt model and story model

--- routes/api/prompts.js
+++ routes/api/prompts.js
@@ -93,6 +93,67 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/v1/promipt/like/:id
+// @desc    Like a prompt
+// @access  PRIVATE
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const prompt = await Prompt.findById(req.params.id);
+
+    // check if prompt has already been liked
+    if (
+      prompt.likes.filter(like => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Prompt already liked' });
+    }
+
+    // unshift user to likes
+    prompt.likes.unshift({ user: req.user.id });
+
+    // update db
+    await prompt.save();
+
+    res.json(prompt.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/v1/prompt/unlike/:id
+// @desc    Unlike a story
+// @access  PRIVATE
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const prompt = await Prompt.findById(req.params.id);
+
+    // check if story has already been liked
+    if (
+      prompt.likes.filter(like => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Prompt has not yet been liked' });
+    }
+
+    // get remove index
+    const removeIndex = prompt.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    // slice out of likes array
+    prompt.likes.splice(removeIndex, 1);
+
+    // update db
+    await prompt.save();
+
+    res.json(prompt.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 // @route   POST api/v1/prompt
 // @desc    CREATE prompt
 // @access  Public
@@ -128,7 +189,7 @@ router.post(
       }
 
       // destructure request body
-      const { name, penName, title, genre, content } = req.body;
+      const { title, genre, content } = req.body;
 
       // init new instance of prompt
       const newPrompt = {};
@@ -136,8 +197,8 @@ router.post(
       newPrompt.title = title;
       newPrompt.genre = genre;
       newPrompt.content = content;
-      if (name) newPrompt.name = user.name;
-      if (penName) newPrompt.penName = profile.penName;
+      if (profile.penName) newPrompt.penName = profile.penName;
+      if (!profile.penName) newPrompt.name = user.name;
 
       // save prompt to db and push prompt.id to profile
       const prompt = new Prompt(newPrompt);
@@ -151,5 +212,95 @@ router.post(
     }
   }
 );
+
+// @route   POST api/v1/promipt/comment/:id
+// @desc    CREATE a comment on a prompt
+// @access  Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    check('text', 'Text is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    // if errors from validation exists
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const prompt = await Prompt.findById(req.params.id);
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      // if no profile
+      if (!profile) {
+        return res.status(400).json({
+          msg: 'Must first create a profile before submitting stories'
+        });
+      }
+
+      // destructure request body
+      const { name, penName, text } = req.body;
+
+      // init new instance of story
+      const newComment = {};
+      newComment.user = req.user.id;
+      newComment.text = text;
+      if (name) newComment.name = user.name;
+      if (penName) newComment.penName = profile.penName;
+
+      // unshift new comment and update db
+      await prompt.comments.unshift(newComment);
+      await prompt.save();
+
+      return res.status(200).json(prompt.comments);
+    } catch (err) {
+      console.warn(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route   DELETE api/v1/story/comment/:id/:comment_id
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const prompt = await Prompt.findById(req.params.id);
+
+    // Get comment
+    const comment = prompt.comments.find(
+      com => com.id === req.params.comment_id
+    );
+
+    // Check if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Make sure comment requesting deletion is from comment user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // get index of comment
+    const removeIndex = prompt.comments
+      .map(com => com.user.toString())
+      .indexOf(req.user.id);
+
+    // remove from comment array
+    prompt.comments.splice(removeIndex, 1);
+
+    // update db and return response
+    await prompt.save();
+    res.json(prompt.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
--- routes/api/stories.js
+++ routes/api/stories.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// @route   GET api/v1/story
+// @route   GET api/v1/story/mystories
 // @desc    Get logged in users stories
 // @access  PRIVATE
 router.get('/mystories', auth, async (req, res) => {
@@ -94,6 +94,67 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/v1/story/like/:id
+// @desc    Like a story
+// @access  PRIVATE
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const story = await Story.findById(req.params.id);
+
+    // check if story has already been liked
+    if (
+      story.likes.filter(like => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Story already liked' });
+    }
+
+    // unshift user to likes
+    story.likes.unshift({ user: req.user.id });
+
+    // update db
+    await story.save();
+
+    res.json(story.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/v1/story/unlike/:id
+// @desc    Unlike a story
+// @access  PRIVATE
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const story = await Story.findById(req.params.id);
+
+    // check if story has already been liked
+    if (
+      story.likes.filter(like => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Story has not yet been liked' });
+    }
+
+    // get remove index
+    const removeIndex = story.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    // slice out of likes array
+    story.likes.splice(removeIndex, 1);
+
+    // update db
+    await story.save();
+
+    res.json(story.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 // @route   POST api/v1/story
 // @desc    CREATE story
 // @access  Public
@@ -129,7 +190,7 @@ router.post(
       }
 
       // destructure request body
-      const { name, penName, title, genre, content } = req.body;
+      const { title, genre, content } = req.body;
 
       // init new instance of story
       const newStory = {};
@@ -137,8 +198,8 @@ router.post(
       newStory.title = title;
       newStory.genre = genre;
       newStory.content = content;
-      if (name) newStory.name = user.name;
-      if (penName) newStory.penName = profile.penName;
+      if (profile.penName) newStory.penName = profile.penName;
+      if (!profile.penName) newStory.name = user.name;
 
       // save story to db and push story.id to profile
       const story = new Story(newStory);
@@ -152,5 +213,95 @@ router.post(
     }
   }
 );
+
+// @route   POST api/v1/story/comment/:id
+// @desc    CREATE a comment on story
+// @access  Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    check('text', 'Text is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    // if errors from validation exists
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const story = await Story.findById(req.params.id);
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      // if no profile
+      if (!profile) {
+        return res.status(400).json({
+          msg: 'Must first create a profile before submitting stories'
+        });
+      }
+
+      // destructure request body
+      const { name, penName, text } = req.body;
+
+      // init new instance of story
+      const newComment = {};
+      newComment.user = req.user.id;
+      newComment.text = text;
+      if (name) newComment.name = user.name;
+      if (penName) newComment.penName = profile.penName;
+
+      // unshift new comment and update db
+      await story.comments.unshift(newComment);
+      await story.save();
+
+      return res.status(200).json(story.comments);
+    } catch (err) {
+      console.warn(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route   DELETE api/v1/story/comment/:id/:comment_id
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const story = await Story.findById(req.params.id);
+
+    // Get comment
+    const comment = story.comments.find(
+      com => com.id === req.params.comment_id
+    );
+
+    // Check if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Make sure comment requesting deletion is from comment user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // get index of comment
+    const removeIndex = story.comments
+      .map(com => com.user.toString())
+      .indexOf(req.user.id);
+
+    // remove from comment array
+    story.comments.splice(removeIndex, 1);
+
+    // update db and return response
+    await story.save();
+    res.json(story.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;

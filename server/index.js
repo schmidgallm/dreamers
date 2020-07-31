df4b636fae7143f2e8925b3a596b6d4e29023192
schmidgallm <schmidgallm@outlook.com>
2020-07-30 21:06:22 -0700

    moved cors call as first function and removed unused vars

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-const path = require('path');
 const connectDB = require('../db/index.js');
 const userRoute = require('../routes/api/users');
 const authRoute = require('../routes/api/auth');
 const profileRoute = require('../routes/api/profiles');
 const storyRoute = require('../routes/api/stories');
 const promptRoute = require('../routes/api/prompts');
+
+// cors middleware
+app.use(cors());
 
 // init app
 const app = express();
@@ -19,9 +21,6 @@ app.use(logger('dev'));
 
 // Body Parser Middleware
 app.use(express.json({ extended: true }));
-
-// cors middleware
-app.use(cors());
 
 // Init database connection
 connectDB();
